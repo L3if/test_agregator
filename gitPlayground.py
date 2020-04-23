1727f15b1320edeaaaa5b59ae3751162b633ead8
tester <tester@iMac-tester.Dlink>
2020-04-23 17:38:02 +0300

    commit iteration works *happy cries*

--- gitPlayground.py
+++ gitPlayground.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-#import pygit2
-from pygit2 import Repository
-from pygit2 import GIT_SORT_TOPOLOGICAL
+# import pygit2
+from pygit2 import Repository, Remote
+from pygit2 import GIT_SORT_TOPOLOGICAL, GIT_SORT_REVERSE
 import os.path
 from os import path
 import sys
 
-#get arg verify path existence
+# get arg verify path existence
 
-if  len(sys.argv) == 1:
+if len(sys.argv) == 1:
     print('path should be specified')
     exit()
 
@@ -19,20 +19,32 @@ else:
 
 repo = Repository(sys.argv[1])
 
-print(list(repo.branches))
+# print(list(repo.branches))
 
-#branch = repo.lookup_branch('develop')
-#ref = repo.lookup_reference(branch.name)
+# branch = repo.lookup_branch('develop')
+# ref = repo.lookup_reference(branch.name)
 
 branch = repo.lookup_branch('develop')
 ref = repo.lookup_reference(branch.name)
 repo.checkout(ref)
 pass
-for commit in repo.walk(repo.head.target):
+
+
+#def function get_commit_list(first_commit_id, last_commit_id):
+for commit in repo.walk(repo.head.target, GIT_SORT_REVERSE):
     print(commit.id)
-    #diffs = repo.diff('HEAD', commit.id)
-    #patches = [p for p in diffs]
-    #for p in patches:
-    #    print
-    #    print(p.text)
-    #    pass                                                                                                                                                                                                                                                                         
+    if commit.parents:
+        diffs = repo.diff(commit.parents[0], commit)
+        print (len(diffs))
+        for p in diffs:
+            print(p.text)
+        
+        
+        
+        pass
+    '''diffs = repo.diff('HEAD', commit.id)
+    patches = [p for p in diffs]
+    for p in patches:
+        print
+        print(p.text)
+        pass'''                                                                        
