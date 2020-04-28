b58eaef6bc68564b2578bb52379b517ce923b685
tester <tester@iMac-tester.Dlink>
2020-04-29 01:21:49 +0300

    migrated to args

--- gitPlayground.py
+++ gitPlayground.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# import pygit2
+import pygit2
 from pygit2 import Repository, Remote, Oid
 from pygit2 import GIT_SORT_TOPOLOGICAL, GIT_SORT_REVERSE
 from os import path
@@ -7,47 +7,6 @@ import sys
 import structparser
 import pprint
 import argparse
-
-# get arg
-
-# args parsed here, current args are: --path <path to repo> --branch
-parser = argparse.ArgumentParser(description="Tool to get and parse changed structures and instrunctuions in any .cpp repository")
-
-parser.add_argument("-p", "--path", help="path to a repository",
-                    type=str)
-parser.add_argument("-b", "--branch", help="branch name to mine changes",
-                    type=str)
-parser.add_argument("-s", "--start", help="full commit id to start from",
-                    type=str)
-parser.add_argument("-f", "--finish", help="full commit id to end with, HEAD used if not specified",
-                    type=str)
-
-args = parser.parse_args()
-if args.path:
-    # verify path existence
-    if path.exists(args.path) and args.path.endswith('.git'):
-        proj_path = args.path[:-4]
-        repo = Repository(args.path)
-        if args.branch and args.branch in list(repo.branches.local):
-            branch = repo.lookup_branch(list(repo.branches.local)[1])
-            ref = repo.lookup_reference(branch.name)
-            repo.checkout(ref)
-            print('Current Branch:')
-            print(repo.head.shorthand)
-            if args.start:
-                commit_id_list = get_commit_id_list()
-
-        else:
-            #print avaliable branches
-            print('Specify one of local avaliable local branches:')
-            print(*list(repo.branches.local), sep="\n")
-        
-else:
-    print('path ether not exis or it\'s not a repo')
-    exit()
-
-
-print(structparser.get_cpp_funcs('/Users/tester/aggr/fuzzingpintool/Tracker.cpp'))
 
 
 def get_commit_id_list(last_commit_id=None, first_commit_id=None):
@@ -60,17 +19,6 @@ def get_commit_id_list(last_commit_id=None, first_commit_id=None):
     
     modified_commit_list = commit_list[commit_list.index(first_commit_id_Oid):]
     return modified_commit_list
- 
-    
-commit_id_list = get_commit_id_list()
-print(commit_id_list)
-
-commit_id_list = get_commit_id_list('250c11c6fff841220d93db5379659342a038436d',
-                                    '4766ee44dcf75a28ac728d9292d2f09419982ac5')
-
-print(commit_id_list)
-start_commit = repo.get(commit_id_list[0])
-last_commit = repo.get(commit_id_list[-1])
 
 
 def get_hunks_in_commits(start_commit, last_commit):
@@ -81,14 +29,60 @@ def get_hunks_in_commits(start_commit, last_commit):
         for hunk in patch.hunks:
             changed_func_dict[patch.delta.new_file.path].append(hunk)
     return changed_func_dict
-    
-hunks_dict = get_hunks_in_commits(start_commit, last_commit)
 
-for key in hunks_dict:
+# get arg
+
+# args parsed here, current args are: --path <path to repo> --branch
+
+
+parser = argparse.ArgumentParser(description="Tool to get and parse changed structures and instrunctuions in any .cpp repository")
+
+parser.add_argument("-p", "--path", help="path to a repository",
+                    type=str)
+parser.add_argument("-b", "--branch", help="branch name to mine changes",
+                    type=str)
+parser.add_argument("-l", "--list", help="list full commit list in branch",
+                    action='store_true')
+parser.add_argument("-s", "--start", help="full commit id to start from",
+                    type=str)
+parser.add_argument("-f", "--finish", help="full commit id to end with, HEAD used if not specified",
+                    type=str)
+
+args = parser.parse_args()
+if args.path:
+    # verify path existence
+    if path.exists(args.path) and args.path.endswith('.git'):
+        proj_path = args.path[:-4]
+        repo = Repository(args.path)
+        if args.branch and args.branch in list(repo.branches.local):
+            branch = repo.lookup_branch(list(repo.branches.local)[list(repo.branches.local).index(args.branch)])
+            ref = repo.lookup_reference(branch.name)
+            repo.checkout(ref)
+            print('Current Branch:')
+            print(repo.head.shorthand)
+            if args.list:
+                pprint.pprint(get_commit_id_list(args.finish, args.start))
+            if args.start and args.finish:
+                pprint.pprint(get_hunks_in_commits(args.start, args.finish))
+        else:
+            # pyprint avaliable branches
+            print('Specify one of local avaliable local branches:')
+            print(*list(repo.branches.local), sep="\n")
+else:
+    print('path ether not exis or it\'s not a repo')
+    exit()
+
+
+# print(structparser.get_cpp_funcs('/Users/tester/aggr/fuzzingpintool/Tracker.cpp'))
+
+
+# hunks_dict = get_hunks_in_commits(start_commit, last_commit)
+
+'''for key in hunks_dict:
     for hunk in hunks_dict[key]:
         print(hunk.header)
 
 
 
-pprint.pprint(get_hunks_in_commits(start_commit, last_commit))
+pprint.pprint(get_hunks_in_commits(start_commit, last_commit))'''
 
