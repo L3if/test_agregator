c0bb12e7f72bb0dd1a2df7a6601f398eabd034eb
tester <tester@iMac-tester.Dlink>
2020-05-02 12:50:36 +0300

    Ondre ne psiHUI

--- utils.py
+++ utils.py
@@ -2,10 +2,7 @@ import subprocess
 
 
 def git_checkout(pwd, commit_id):
-    cmd = []
-    cmd.append('git')
-    cmd.append('checkout')
-    cmd.append(commit_id)
+    cmd = ['git', 'checkout', commit_id]
     p = subprocess.Popen(cmd, cwd=pwd, stdout=PIPE)
     p.wait()
     return p.returncode
